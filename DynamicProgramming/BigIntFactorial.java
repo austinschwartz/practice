9ffda7133bb027f123f8fdab69ee40d0649fb12d
Austin Schwartz <schwar12@purdue.edu>
2013-11-01 12:21:42 -0400

    moved

--- DynamicProgramming/BigIntFactorial.java
+++ DynamicProgramming/BigIntFactorial.java
@@ -2,30 +2,30 @@ import java.lang.System;
 import java.util.*;
 import java.math.*;
 
-public class Memoization {
-    
+public class BigIntFactorial {
+
     static Hashtable<Integer, BigInteger> factorials = new Hashtable<Integer, BigInteger>();
-    
+
     public static BigInteger fact(int n) {
-        if (n == 1) 
+        if (n == 1)
             return BigInteger.ONE;
-        else 
+        else
             return (new BigInteger(Integer.toString(n)).multiply(fact(n-1)));
     }
-    
+
     public static BigInteger fact2(int n) {
-        
+
         BigInteger x = factorials.get(n);
-        if (x != null) 
+        if (x != null)
             return x;
-        if (n <= 1) 
+        if (n <= 1)
             return BigInteger.ONE;
-        
+
         BigInteger j = new BigInteger(Integer.toString(n));
         factorials.put(n, j.multiply(fact2(n-1)));
         return factorials.get(n);
     }
-    
+
     public static void main(String[] args) {
         System.out.println(fact(100));
         System.out.println(fact(99));
