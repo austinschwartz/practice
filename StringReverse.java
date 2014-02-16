8454664d169ddc9ace785355176ef2fbf4e01df2
Austin Schwartz <schwar12@purdue.edu>
2014-02-16 11:47:58 -0500

    String Reverse

--- StringReverse.java
+++ StringReverse.java
@@ -25,10 +25,41 @@ class StringReverse {
         return sb.toString();
     }
 
+    public static char[] StringReverseInplace(char[] str)
+    {
+        for (int i = 0, j = str.length - 1; i < str.length / 2; i++, j--)
+        {
+            char temp = str[i];
+            str[i] = str[j];
+            str[j] = temp;
+        }
+        return str;
+    }
+
+    public static char[] StringReverseInplaceRecursive(char[] str)
+    {
+        return StringReverseInplaceRecursive(str, 0, str.length-1);
+    }
+
+    public static char[]  StringReverseInplaceRecursive(char[] str, int left, int right)
+    {
+        if (left >= right)
+            return str;
+        else
+        {
+            char temp = str[left];
+            str[left] = str[right];
+            str[right] = temp;
+            return StringReverseInplaceRecursive(str, left+1, right-1);
+        }
+    }
+
     public static void main(String[] args)
     {
         String a = "Hello this is a string.";
         System.out.println(StringReverse(a));
         System.out.println(StringReverse2(a));
+        System.out.println(StringReverseInplace(a.toCharArray()));
+        System.out.println(StringReverseInplaceRecursive(a.toCharArray()));
     }
 }
